2710207baa402fc3dbb97c101cdc814d0622e319
Tarek <tazihad@gmail.com>
2021-07-24 03:47:31 +0600

    Update 3.reset-ip-tables.sh

--- 3.reset-ip-tables.sh
+++ 3.reset-ip-tables.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 iptables -F
-iptables -t nat -F
-iptables -t mangle -F
+iptables -Z
 iptables -X
+iptables -t nat -F
+iptables -t nat -Z
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -Z
+iptables -t mangle -X
 killall redsocks
 sudo fuser -k 12345/tcp
