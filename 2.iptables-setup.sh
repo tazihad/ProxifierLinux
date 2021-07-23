2f5c1520653d32344cc9b651c1bbd3c07f22f4ab
Tarek <tazihad@gmail.com>
2021-07-24 02:24:34 +0600

    Update 2.iptables-setup.sh

--- 2.iptables-setup.sh
+++ 2.iptables-setup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+# Create new chain
 iptables -t nat -N REDSOCKS
 
+# Ignore LANs and some other reserved addresses.
 iptables -t nat -A REDSOCKS -d 0.0.0.0/8 -j RETURN
 iptables -t nat -A REDSOCKS -d 10.0.0.0/8 -j RETURN
 iptables -t nat -A REDSOCKS -d 127.0.0.0/8 -j RETURN
@@ -11,10 +13,13 @@ iptables -t nat -A REDSOCKS -d 192.168.0.0/16 -j RETURN
 iptables -t nat -A REDSOCKS -d 224.0.0.0/4 -j RETURN
 iptables -t nat -A REDSOCKS -d 240.0.0.0/4 -j RETURN
 
+# Anything else should be redirected to port 12345
 iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 12345
 
+# Redirect all HTTP and HTTPS outgoing packets through Redsocks
 iptables -t nat -A OUTPUT -p tcp --dport 443 -j REDSOCKS
 iptables -t nat -A OUTPUT -p tcp --dport 80 -j REDSOCKS
 
+# Defining the following rules in the PREROUTING chain. For redirecting incomming packets to the REDSOCKS chain. 
 iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDSOCKS
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDSOCKS
